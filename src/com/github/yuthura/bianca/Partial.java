7d0a90b07e33dd4f95ba98b45cb3454c9d562945
Yuthura <yuthura@hotmail.com>
2014-02-09 21:32:10 +0100

    Added recognition for more types on Partial#wrap (long, float, boolean, local date, local time, local date time).

--- src/com/github/yuthura/bianca/Partial.java
+++ src/com/github/yuthura/bianca/Partial.java
@@ -1,6 +1,7 @@
 package com.github.yuthura.bianca;
 
 import java.sql.*;
+import java.time.*;
 
 public interface Partial {
 	public void buildStatement(StringBuilder sb);
@@ -34,14 +35,32 @@ public interface Partial {
 		} else if(object instanceof Integer) {
 			return Type.INTEGER.asPartial((Integer)object);
 
+		} else if(object instanceof Long) {
+			return Type.LONG.asPartial((Long)object);
+
+		} else if(object instanceof Float) {
+			return Type.FLOAT.asPartial((Float)object);
+
 		} else if(object instanceof Double) {
 			return Type.DOUBLE.asPartial((Double)object);
+
+		} else if(object instanceof Boolean) {
+			return Type.BOOLEAN.asPartial((Boolean)object);
 
 		} else if(object instanceof String) {
 			return Type.STRING.asPartial((String)object);
 
+		} else if(object instanceof LocalDate) {
+			return Type.DATE.asPartial((LocalDate)object);
+
+		} else if(object instanceof LocalTime) {
+			return Type.TIME.asPartial((LocalTime)object);
+
+		} else if(object instanceof LocalDateTime) {
+			return Type.DATETIME.asPartial((LocalDateTime)object);
+
 		} else {
-			throw new CoercionException();
+			throw new CoercionException(object.getClass().getName());
 		}
 	}
 }
