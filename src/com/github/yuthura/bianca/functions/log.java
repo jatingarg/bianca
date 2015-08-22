a9530c40c98fc4cf551ecda2450371ec34dd02e7
jatingarg <2014csb1017@iitrpr.ac.in>
2015-08-22 18:27:29 +0530

    added LOG function

---
+++ src/com/github/yuthura/bianca/functions/log.java
@@ -0,0 +1,10 @@
+package com.github.yuthura.bianca.functions;
+
+public class log extends UnaryFunction {
+	public static final String OPERATOR = "LOG";
+
+	public log(Object argument) {
+		super(OPERATOR, argument);
+	}
+
+}
